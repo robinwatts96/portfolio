de1d1c0d8598646663978ae9c6a0ef239d3fe46e
robinwatts96 <robinwatts96@gmail.com>
2020-08-31 12:29:00 +0100

    ...

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-// Portfolio
-document.getElementById("web-app").addEventListener("click", function(){
-    document.getElementById("web-app-div").innerHTML = "Hello World";
-  });
+
+  // Variables =============================================
+const projects = document.querySelectorAll('.project');
+// loop over each project and attach event listner
+projects.forEach( project => project.addEventListener('click', function (){ showProjectDesc(this); }));
+// Helpers =============================================
+const showProjectDesc = project => {
+}
