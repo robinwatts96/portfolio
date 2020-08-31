f614620b4fe19a5b72e894ac23a4a7d8e230ef3e
robinwatts96 <robinwatts96@gmail.com>
2020-08-31 17:17:43 +0100

    Update - footer and contacted added

--- js/script.js
+++ js/script.js
@@ -11,11 +11,15 @@ const showProjectDesc = project => {
     if (projectTitle === p.title) {
       const {title, desc, gitHub} = p;
       project.innerHTML = 
-        `<div class="project-inner-html">
-          <h4>${title}</h4>
-          <p>${desc}</p>
-          <a id="project-github" href="gitHub" target="_blank">Git Hub</a>
-          <a id="project-demo" href="Demo" target="_blank">Demo</a>
+        `<div class="project-inner-html-container">
+          <div class="project-inner-html">
+            <h4>${title}</h4>
+            <p>${desc}</p>
+          </div>
+          <div class="project-button-container"> 
+            <a id="project-github" href="gitHub" target="_blank">Git Hub</a>
+            <a id="project-demo" href="Demo" target="_blank">Demo</a>
+          </div>
         </div>`;
     }
   });
