dd56f22addda374a7dd6beb66f2e768807922ca1
robinwatts96 <robinwatts96@gmail.com>
2020-08-31 14:26:25 +0100

    Projects in (woooo)

---
+++ js/data.js
@@ -0,0 +1,38 @@
+const projects = [
+    {
+      title: 'Web App Dashboard',
+      desc: 'An interactive web app designed from a provided mockup. For this project, vanilla JavaScript was used alongside Chart.js for the pages data and functionality.',
+      gitHub: 'https://github.com/robinwatts96/webapp-dashboard',
+      Demo: 'https://github.com/robinwatts96/webapp-dashboard'
+    },
+    {
+        title: 'API Employee Directory',
+        desc: 'An employee directory that displays 12 random \"Employee\'s\" data by communicating with a third-party API.',
+        gitHub: 'https://github.com/robinwatts96/webapp-dashboard',
+        Demo: 'https://github.com/robinwatts96/webapp-dashboard'
+    },
+    {
+        title: 'Game Show App',
+        desc: 'A word guessing game that uses JavaScript to select a random phrase from an array of choices. The user then selects letters and attempts to guess the prhase, before they run out of lives (hearts).',
+        gitHub: 'https://github.com/robinwatts96/webapp-dashboard',
+        Demo: 'https://github.com/robinwatts96/webapp-dashboard'
+    },
+    {
+        title: 'Interactive Photo Gallery',
+        desc: 'An interactive photo gallery that uses vanilla JavaScript for search functionality alongside jQuery and a lightbox jQuery plugin.',
+        gitHub: 'https://github.com/robinwatts96/webapp-dashboard',
+        Demo: 'https://github.com/robinwatts96/webapp-dashboard'
+      },
+      {
+        title: 'Online Registration Form',
+        desc: 'A newsletter signup form built via HTML forms and designed for mobile first, which expands responsibly depending on what device you are viewing on.',
+        gitHub: 'https://github.com/robinwatts96/webapp-dashboard',
+        Demo: 'https://github.com/robinwatts96/webapp-dashboard'
+      },
+      {
+        title: 'Full Stack Conf',
+        desc: 'A site styled and structured used Bootstrap. Responsive utility classes are used amongst other Bootstrap features.',
+        gitHub: 'https://github.com/robinwatts96/webapp-dashboard',
+        Demo: 'https://github.com/robinwatts96/webapp-dashboard'
+      }   
+  ];
--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-
-  // Variables =============================================
-const projects = document.querySelectorAll('.project');
+// Variables =============================================
+const portfolio = document.querySelectorAll('.project');
 // loop over each project and attach event listner
-projects.forEach( project => project.addEventListener('click', function (){ showProjectDesc(this); }));
+portfolio.forEach( project => project.addEventListener('click', function (){ showProjectDesc(this); }));
 // Helpers =============================================
 const showProjectDesc = project => {
+  // get the data-title attribute value
+  const projectTitle = project.getAttribute('data-title');
+  // loop over the projects array and find the project that matches
+  projects.filter( p => {
+    if (projectTitle === p.title) {
+      const {title, desc, gitHub} = p;
+      project.innerHTML = 
+        `<div>
+          <h2>${title}</h2>
+          <p>${desc}</p>
+          <a href="gitHub" target="_blank">Git Hub</a>
+          <a href="Demo" target="_blank">Demo</a>
+        </div>`;
+    }
+  });
 }
-
-...
