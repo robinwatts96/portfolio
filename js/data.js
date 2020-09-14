0eab488e1326a97d7229c5b972ea81b834116e58
robinwatts96 <robinwatts96@gmail.com>
2020-09-14 21:53:33 +0100

    Done

--- js/data.js
+++ js/data.js
@@ -3,36 +3,36 @@ const projects = [
       title: 'Web App Dashboard',
       desc: 'An interactive web app designed from a provided mockup. For this project, vanilla JavaScript was used alongside Chart.js for the pages data and functionality.',
       gitHub: 'https://github.com/robinwatts96/webapp-dashboard',
-      liveDemo: 'https://github.com/robinwatts96/webapp-dashboard'
+      liveDemo: 'https://webapp-dashboard.vercel.app/'
     },
     {
         title: 'API Employee Directory',
         desc: 'An employee directory that displays 12 random \"Employee\'s\" data by communicating with a third-party API.',
         gitHub: 'https://github.com/robinwatts96/employee-dictionary',
-        liveDemo: 'https://github.com/robinwatts96/webapp-dashboard'
+        liveDemo: 'https://employee-dictionary.vercel.app/'
     },
     {
         title: 'Game Show App',
         desc: 'A word guessing game that uses JavaScript to select a random phrase from an array of choices. The user then selects letters and attempts to guess the phrase, before they run out of lives (hearts).',
         gitHub: 'https://github.com/robinwatts96/game-show-app',
-        liveDemo: 'https://github.com/robinwatts96/webapp-dashboard'
+        liveDemo: 'https://game-show-app.vercel.app/'
     },
     {
         title: 'Interactive Photo Gallery',
         desc: 'An interactive photo gallery that uses vanilla JavaScript for search functionality alongside jQuery and a lightbox jQuery plugin.',
         gitHub: 'https://github.com/robinwatts96/interactive-photo-gallery',
-        liveDemo: 'https://github.com/robinwatts96/webapp-dashboard'
+        liveDemo: 'https://interactive-photo-gallery.vercel.app/'
       },
       {
         title: 'Online Registration Form',
         desc: 'A newsletter signup form built via HTML forms and designed for mobile first, which expands responsibly depending on what device you are viewing on.',
         gitHub: 'https://github.com/robinwatts96/Online-Registration-Form',
-        liveDemo: 'https://github.com/robinwatts96/webapp-dashboard'
+        liveDemo: 'https://online-registration-form.vercel.app/'
       },
       {
         title: 'Full Stack Conf',
         desc: 'A site styled and structured used Bootstrap. Responsive utility classes are used amongst other Bootstrap features.',
         gitHub: 'https://github.com/robinwatts96/full-stack-conf',
-        liveDemo: 'https://robinwatts96.github.io/full-stack-conf'
+        liveDemo: 'https://full-stack-conf.vercel.app/'
       }   
   ];
